c2d15803998161fe0e4c39506aa096184d6dc1a0
Нюньков Олег Владимирович <Nyunkov.O@dns-shop.ru>
2022-05-26 18:48:43 +0600

    dev: refactored constructor

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -15,15 +15,21 @@ const BurgerConstructor = ({ openOrderModal }) => {
 
   return (
     <section className='pt-25 pl-4'>
-      <div className='pl-8 mr-4 mb-4'>
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={1255}
-          thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-        />
-      </div>
+      {
+        data.map((item) => {
+          if (item.name === 'Краторная булка N-200i') {  
+            return ( 
+              <div key={item._id} className='pl-8 mr-4 mb-4'>
+                <ConstructorElement
+                  type="top"
+                  isLocked={true}
+                  text={`${item.name} ${'(верх)'}`}
+                  price={item.price}
+                  thumbnail={item.image}
+                />
+              </div>
+            )}})
+        }      
       <div className={`${Constructor.constructor__elements} mb-4 pr-2`}>
         {
           data.map((item) => {
@@ -44,15 +50,20 @@ const BurgerConstructor = ({ openOrderModal }) => {
           })
         }
       </div>
-      <div className='pl-8 mb-10 mr-4'>
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (низ)"
-          price={1255}
-          thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-        />
-      </div>
+      {
+        data.map((item) => {
+          if (item.name === 'Краторная булка N-200i') {
+            return (
+              <div key={item._id} className='pl-8 mr-4 mb-4'>
+                <ConstructorElement
+                  type="bottom"
+                  isLocked={true}
+                  text={`${item.name} ${'(низ)'}`}
+                  price={item.price}
+                  thumbnail={item.image}
+                />
+              </div>
+            )}})}
       <div className={`${Constructor.constructor__total} mr-4`}>
         <div className={`${Constructor.constructor__price} mr-10`}>
           <p className="text text_type_digits-medium mr-2">610</p>
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const apiUrl = 'https://norma.nomoreparties.space/api/ingredients';
+const orderApiUrl = 'https://norma.nomoreparties.space/api/orders';
+
 const checkResponse = (res) => {
   if (res.ok) {
     return res.json();
@@ -13,6 +15,16 @@ export const api = () => {
   return fetch(apiUrl, {
     headers: {
       'Content-Type': 'application/json'
+    }
+  })
+    .then(checkResponse)
+}
+
+export const orderApi = () => {
+  return fetch(orderApiUrl, {
+    headers: {
+      'Content-Type': 'application/json',
+      'ingredients': ["609646e4dc916e00276b286e","609646e4dc916e00276b2870"]
     }
   })
     .then(checkResponse)
