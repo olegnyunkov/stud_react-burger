2d0d9c487a61a964c6b679f67c2c82e37d44f999
Oleg Nyunkov <nvo84@yandex.ru>
2022-07-20 00:10:28 +0600

    dev: feed in process

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -5,11 +5,18 @@ import FeedItem from "../components/feed-item/feed-item";
 export const FeedPage = () => {
 
   return (
-    <div className={PagesStyles.feed}>
-      <h2 className={`${PagesStyles.feed__title} text text_type_main-large`}>Лента заказов</h2>
-      <div className={PagesStyles.feed__container}>
-        <FeedItem />
-        <FeedItem />
+    <div className={PagesStyles.feed__info}>
+      <div className={PagesStyles.feed}>
+        <h2 className={`${PagesStyles.feed__title} text text_type_main-large`}>Лента заказов</h2>
+        <div className={PagesStyles.feed__container}>
+          <FeedItem/>
+          <FeedItem/>
+          <FeedItem/>
+          <FeedItem/>
+        </div>
+      </div>
+      <div>
+        <p className="text text_type_digits-large">28 752</p>
       </div>
     </div>
   )
