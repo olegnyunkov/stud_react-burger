1b180f56217c89d0f54c8d7394de4fd2a6631852
Oleg Nyunkov <nvo84@yandex.ru>
2022-07-21 00:47:47 +0600

    dev: feed in process

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -6,7 +6,8 @@ export const FeedPage = () => {
 
   return (
     <div className={PagesStyles.feed__info}>
-      <div className={PagesStyles.feed}>
+
+      <div className={`${PagesStyles.feed} mr-15`}>
         <h2 className={`${PagesStyles.feed__title} text text_type_main-large`}>Лента заказов</h2>
         <div className={PagesStyles.feed__container}>
           <FeedItem/>
@@ -15,9 +16,30 @@ export const FeedPage = () => {
           <FeedItem/>
         </div>
       </div>
-      <div>
-        <p className="text text_type_digits-large">28 752</p>
+
+      <div className={PagesStyles.feed__stats}>
+        <div className={PagesStyles.feed__status}>
+          <div className={PagesStyles.feed__ready}>
+            <p className='text text_type_main-medium mb-6'>Готовы:</p>
+            <p className='text text_type_digits-default mb-2'>034533</p>
+            <p className='text text_type_digits-default'>034533</p>
+          </div>
+          <div className={PagesStyles.feed__process}>
+            <p className='text text_type_main-medium mb-6'>В работе:</p>
+            <p className='text text_type_digits-default mb-2'>034538</p>
+            <p className='text text_type_digits-default'>034541</p>
+          </div>
+        </div>
+        <div>
+          <p className='text text_type_main-medium'>Выполнено за все время:</p>
+          <p className="text text_type_digits-large">28 752</p>
+        </div>
+        <div>
+          <p className='text text_type_main-medium'>Выполнено за сегодня:</p>
+          <p className="text text_type_digits-large">138</p>
+        </div>
       </div>
+
     </div>
   )
 }
